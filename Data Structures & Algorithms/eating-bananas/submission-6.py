70726c94caf7f1f3d85a18989408dd56cbed39d3
Adam Vakar <Adwerse.2005@gmail.com>
2026-09-05 14:11:55 +0100

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        def feasible(k): # Where k is bananas/hour
+            hours = sum(math.ceil(p / k) for p in piles) # For each pile in array piles     looking for it's hours in which we can eat them
+            return hours <= h #Checking if we ulozhilis in our max h
+
+        l, r = 1, max(piles)
+        while l < r:
+            mid = (l + (r - 1)) // 2
+
+            if feasible(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
+
+
